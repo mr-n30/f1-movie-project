f64533d4d64d17c2f9ea777626d71a0284422cdf
mr-n30 <mr-n30@localhost>
2025-08-17 04:22:14 -0500

    Added background video

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,9 @@ function App() {
 
   return (
     <div className='main-container'>
+      <iframe src="https://www.youtube.com/embed/CT2_P2DZBR0?si=7TjVZhcQWuPzc-eE&autoplay=1&mute=1&loop=1&playlist=CT2_P2DZBR0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <Navbar />
-      <button className='watch-trailer-button'>Watch Trailer</button>
+      <button className='watch-trailer-button'><a href='https://youtu.be/CT2_P2DZBR0?si=5X7VW2SAQPvls_a9' target='_blank'>Watch Trailer</a></button>
     </div>
   )
 }
--- frontend/src/assets/components/Navbar.jsx
+++ frontend/src/assets/components/Navbar.jsx
@@ -2,10 +2,10 @@ export default function Navbar() {
     return (
         <nav>
             <ul>
-                <li className="nav-link"><a href="#">Home</a></li>
-                <li className="nav-link"><a href="#">About</a></li>
-                <li className="nav-link"><a href="#">Cast</a></li>
-                <li className="nav-link nav-link-ticket"><a href="#">Tickets</a></li>
+                <li className="nav-link"><a href="https://tv.apple.com/us/movie/f1-the-movie/umc.cmc.3t6dvnnr87zwd4wmvpdx5came">Home</a></li>
+                <li className="nav-link"><a href="https://tv.apple.com/us/movie/f1-the-movie/umc.cmc.3t6dvnnr87zwd4wmvpdx5came">About</a></li>
+                <li className="nav-link"><a href="https://tv.apple.com/us/movie/f1-the-movie/umc.cmc.3t6dvnnr87zwd4wmvpdx5came">Cast</a></li>
+                <li className="nav-link nav-link-ticket"><a className="anchor-ticket" href="https://www.google.com/search?q=f1+movie+tickets" target="_blank">Tickets</a></li>
             </ul>
         </nav>
     )
